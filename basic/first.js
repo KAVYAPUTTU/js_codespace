f7f5a0df02a04bda60fd9b69ee04d0447607b53c
PUTTU KAVYA <kavya.puttu2004@gmail.com>
2025-12-27 11:12:14 +0000

    variables in js

--- basic/first.js
+++ basic/first.js
@@ -1,1 +1,17 @@
 console.log("kavya");
+const accountId = 1234;
+let accountName = "kavya"
+var accountPassword ="12we4"
+let accounttag;//undefined
+
+//const cannot be changed
+// accountId=123;
+// console.log(accountId);
+
+accountName="venu"
+accountPassword="sjwnd4"
+
+//let is block scpoe 
+//prefer not to use var beacause it does not follow the blocscope or functional scope
+console.table([accountName,accountPassword,accounttag])
+
