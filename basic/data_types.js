4744e32dd6fd27e3fbd1a69f9eeeebf6fecfad69
PUTTU KAVYA <kavya.puttu2004@gmail.com>
2025-12-27 11:26:57 +0000

    datatypes

---
+++ basic/data_types.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello") //we are using node js not browser so we can not use the alert statement like this
+
+//code readability must be high
+
+//real documentation of js is ecmascript tc39 and we can refer for mdn for eazy referance
+
+let name="kavya" //string
+let age = 23 //number
+let isLoggedIn = false //bool
+
+//number => 2^53 range
+//bigint
+//string => ""
+//boolean => true/false
+//null =>standalone value--empty
+//undefined =>--no value assigned
+//symbol =>unique
+
+//object 
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefine
+
+
